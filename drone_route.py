9e7ddfa75ecd94dc6bfc3df63f1d5cb92ab83548
Joe <105055495+SullySE16@users.noreply.github.com>
2022-12-15 10:24:07 +0000

    Update and rename README.py to drone_route.py
    This was my first assessment to validate my skills as a python programmer. It took me approximately three weeks to complete in my spare time whilst working a full time job. The assessment was given to me as an incentive to get those in my line of work more 'tech savvy' and I have now developed a further understanding and interest in python programming. 
    
    Please note this will not run without the correct '.txt' files located in the same folder. This is here solely for feedback on my use of syntax and methods.
    
    Appreciate it, much love. 
    
    Chopper

--- drone_route.py
+++ drone_route.py
@@ -1,5 +1,5 @@
 # Drone-Mapping-Assessment
-This was the first validation of my skill set as a python programmer. I'd appreciate any feedback. 
+# This was the first validation of my skill set as a python programmer. I'd appreciate any feedback. 
 
 from IPython.display import clear_output
 
